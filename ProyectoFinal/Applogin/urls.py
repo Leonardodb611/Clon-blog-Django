9d5fe0e9530e9c1ac499c993a8a8b38ae6619fa4
Leonardo <leodb611@gmail.com>
2022-04-11 20:01:07 -0300

    Login y logout

--- ProyectoFinal/Applogin/urls.py
+++ ProyectoFinal/Applogin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from Applogin import views
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
 
     path("register", views.register, name="register"),
-    path("login", views.login_request, name = "login")
+    path("login", views.login_request, name = "login"),
+    path("logout", LogoutView.as_view(template_name="Applogin/gracias.html"), name = "logout")
 ]
