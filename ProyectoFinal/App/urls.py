6a7fba6615f298f2e8bced040b3ab976abd9a3be
Leonardo <leodb611@gmail.com>
2022-04-26 19:47:43 -0300

    Cambios

--- ProyectoFinal/App/urls.py
+++ ProyectoFinal/App/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     
     path("", views.random_blog, name = "inicio"),
     path("inicio", views.random_blog, name = "Inicio"),
-    path("createBlog/", views.crear_blog, name="Crear"),
+    path("create_blog/", views.crear_blog, name="Crear"),
     path("messages", views.mensaje, name = "mensajeria"),
     path("pages", views.leer_blogs, name="leerblogs"),
-    path("eliminarblog/<pk>", views.eliminar_blogs, name="Borrarblog"),
-    path("editBlog/<pk>", views.modificar_blogs, name="Editarblog" ),
-    path("especificBlog/<pk>", views.blog_especifico, name="Blogespecifico" ),
-    path("editProfile", views.editar_perfil, name="EditarPerfil"),
+    path("eliminar/blog/<pk>", views.eliminar_blogs, name="Borrarblog"),
+    path("edit/blog/<pk>", views.modificar_blogs, name="Editarblog" ),
+    path("pages/<pk>", views.blog_especifico, name="Blogespecifico" ),
+    path("edit/profile", views.editar_perfil, name="EditarPerfil"),
     path("profile", views.perfil_usuario, name="Perfil"),
     path("about", views.about, name="About")
     
--- ProyectoFinal/Applogin/urls.py
+++ ProyectoFinal/Applogin/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path("login", views.login_request, name = "login"),
     path("logout", LogoutView.as_view(template_name="Applogin/gracias_logout.html"), name = "logout"),
     path("Avatar", views.agregar_avatar, name="AgregarAvatar" ),
-    path("deleteAvatar", views.borrar_avatar, name="BorrarAvatar"),
+    path("delete/avatar", views.borrar_avatar, name="BorrarAvatar"),
     path("redes", views.agregar_redes, name="Redes"),
-    path("createRedes", views.crear_redes, name="crearRedes"),
-    path("editRedes/<pk>", views.modificar_redes, name="EditarRedes" ),
-    path("delRedes/<pk>", views.eliminar_redes, name="EliminarRedes"),
-    path("changePassword", views.cambiar_contrasenia, name="resetPass")
+    path("create/redes", views.crear_redes, name="crearRedes"),
+    path("edit/redes/<pk>", views.modificar_redes, name="EditarRedes" ),
+    path("del/redes/<pk>", views.eliminar_redes, name="EliminarRedes"),
+    path("change_password", views.cambiar_contrasenia, name="resetPass")
 ]
