8205c5d763980d64f024a386c196b1a6ebcced05
Leonardo <leodb611@gmail.com>
2022-05-01 21:20:13 -0300

    Cambios

--- ProyectoFinal/App/urls.py
+++ ProyectoFinal/App/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     
     path("", views.random_blog, name = "Inicio"),
     path("inicio", views.random_blog, name = "Inicio"),
-    path("create_blog/", views.crear_blog, name="CrearBlog"),
+    path("create_blog", views.crear_blog, name="CrearBlog"),
     path("messages", views.mensaje, name = "Mensajes"),
     path("pages", views.leer_blogs, name="LeerBlogs"),
     path("eliminar/blog/<pk>", views.eliminar_blogs, name="BorrarBlog"),
